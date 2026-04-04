b83df5c7c7edd0d3c402823e17d5352f39028889
Shovan04 <shovanm50@gmail.com>
2026-04-04 19:29:59 +0530

    chore(backend): reorganize config and update dev scripts

--- backend/src/routes/users/route.ts
+++ backend/src/routes/users/route.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { UsersRoutes } from "../../configs/routes/users.js";
 import { validateDto } from "../../middlewares/validateDTOs.js";
 import CreateUserDTO from "../../DTOClasses/users/createUser.js";
+import UserController from "../../controllers/users/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.post(UsersRoutes.CREATE_USER, validateDto(CreateUserDTO), )
+userRouter.post(UsersRoutes.CREATE_USER, validateDto(CreateUserDTO), UserController.createUser)
 
 export default userRouter;
